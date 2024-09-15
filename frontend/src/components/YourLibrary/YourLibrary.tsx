ceddb2a94e170860a3cfb24fe0b73052862d7e06
Roman Micuda <roman.micuda@outlook.com>
2024-09-16 01:25:58 +0200

    feat(component): add interactive pop-ups for playlist managment

--- frontend/src/components/YourLibrary/YourLibrary.tsx
+++ frontend/src/components/YourLibrary/YourLibrary.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import YourLibrarySearchBar from "./YourLibrarySearchBar";
 import YourLibraryList from "./YourLibraryList";
 
 function YourLibrary() {
+  const [showAddPopup, setShowAddPopup] = useState(false);
+  const [showMorePopup, setShowPopUp] = useState(false);
+
+  const handleAddPopup = () => setShowAddPopup((prev) => !prev);
+
+  const handleMorePopup = () => setShowPopUp((prev) => !prev);
+
+  const handleCreatePlaylist = () => {};
+
+  const handleCreatePlaylistFolder = () => {};
   return (
     <div>
       <div>
         <span className="material-symbols-outlined">video_library</span>
         <p>Your Library</p>
-        <span className="material-symbols-outlined">add</span>
+        <span className="material-symbols-outlined" onClick={handleAddPopup}>
+          add
+        </span>
+        {showAddPopup && (
+          <div>
+            <div onClick={handleCreatePlaylist}>
+              <span className="material-symbols-outlined">add_box</span>
+              <p>Create New Playlist</p>
+            </div>
+            <div onClick={handleCreatePlaylistFolder}>
+              <span className="material-symbols-outlined">library_add</span>
+              <p>Create Playlist Folder</p>
+            </div>
+          </div>
+        )}
+        <span className="material-symbols-outlined" onClick={handleMorePopup}>
+          more_horiz
+        </span>
+        {showMorePopup && (
+          <div>
+            <p>Rename</p>
+            <p>Delete</p>
+            <p>Create Playlist</p>
+            <p>Create Folder</p>
+            <p>Move Folder</p>
+          </div>
+        )}
         <span className="material-symbols-outlined">arrow_forward</span>
       </div>
       <div>
