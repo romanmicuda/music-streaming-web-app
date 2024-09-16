253e536112de7986523094ace7f2ac07a12f1af0
Roman Micuda <roman.micuda@outlook.com>
2024-09-16 02:35:01 +0200

    feat(footer): add social media icons and section links

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,9 +2,53 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div>
+        <div>
+          <h3>Company</h3>
+          <p>About</p>
+          <p>Jobs</p>
+          <p>For the Record</p>
+          <p></p>
+        </div>
+
+        <div>
+          <h3>Communities</h3>
+          <p>For Artists</p>
+          <p>Developers</p>
+          <p>Advertising</p>
+          <p>Investors</p>
+          <p>Vendors</p>
+        </div>
+
+        <div>
+          <h3>Useful Links</h3>
+          <p>Support</p>
+          <p>Free Mobile</p>
+          <p>App</p>
+        </div>
+
+        <div>
+          <h3>Spotify Plans</h3>
+          <p>Premium Individual</p>
+          <p>Premium Duo</p>
+          <p>Premium Family</p>
+          <p>Spotify Free</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <p>Legal</p>
+          <p>Safety & Privacy Center</p>
+          <p>Privacy Policy</p>
+          <p>Cookie Settings</p>
+          <p>About Ads</p>
+          <p>Accessibility</p>
+        </div>
+        <p>© 2024 Spotify AB</p>
+      </div>
+    </footer>
   );
 }
 
