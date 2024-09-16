c665dc758b1149187d555f6087cee43c3c1fd322
Roman Micuda <roman.micuda@outlook.com>
2024-09-16 02:59:47 +0200

    refactor(layout): remove Footer component from RootLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
           <YourLibrary />
           <PlayingView />
           {children}
-          <Footer />
         </body>
       </html>
     </ReduxProvider>
